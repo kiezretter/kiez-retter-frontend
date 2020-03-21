3a128eeaa31f736f3495aa5e13c08f09830701d7
Celal Basar <mail@celalbasar.com>
2020-03-21 23:00:36 +0100

    Added registration page route

--- src/Routes.js
+++ src/Routes.js
@@ -9,6 +9,7 @@ import DatenschutzView from './views/Datenschutz/Datenschutz.jsx';
 import FAQView from './views/FAQ/FAQ.jsx';
 import ImpressumView from './views/Impressum/Impressum.jsx';
 import UeberUnsView from './views/UeberUns/UeberUns.jsx';
+import RegistrationPage from './views/RegistrationPage/RegistrationPage';
 
 const Routes = () => {
   return (
@@ -21,6 +22,7 @@ const Routes = () => {
         <Route exact path="/faq" component={FAQView} />
         <Route exact path="/impressum" component={ImpressumView} />
         <Route exact path="/ueberuns" component={UeberUnsView} />
+        <Route exact path="/registration" component={RegistrationPage} />
       </Switch>
     </Router>
   );
