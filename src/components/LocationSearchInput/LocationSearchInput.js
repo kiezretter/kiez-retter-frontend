cbe80bbb8fd5f685e8eaa4fa23eb91b601d0baff
Julian Kern <mail@juliankern.com>
2020-03-21 18:05:48 +0100

    new homepage

--- src/components/LocationSearchInput/LocationSearchInput.js
+++ src/components/LocationSearchInput/LocationSearchInput.js
@@ -8,7 +8,27 @@ import PlacesAutocomplete, {
 import { useHistory } from 'react-router-dom';
 import { useStoreContext } from "../../context/StoreContext";
 
-import { TextField } from '@material-ui/core';
+import { 
+  Grid,
+  TextField,
+  InputAdornment,
+  Icon,
+  IconButton,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Paper,
+} from '@material-ui/core';
+
+import './LocationSearchInput.scss';
+
+const locationTypeIcons = {
+  bar: 'local_bar',
+  restaurant: 'restaurant',
+  grocery_or_supermarket: 'local_grocery_store',
+  default: 'location_on',
+};
  
 const LocationSearchInput = () => {
   const history = useHistory();
@@ -35,43 +55,110 @@ const LocationSearchInput = () => {
     radius: 20000,
   }
 
+  const getGeolocation = () => {
+    if (!navigator.geolocation) {
+      alert('Sorry, dein Browser erlaubt keine Abfrage deines Ortes - benutz doch unsere Suchfunktion!');
+    } else {
+      navigator.geolocation.getCurrentPosition(getGeolocationSuccess, alert);
+    }
+  };
+
+  const getGeolocationSuccess = (position) => {
+    console.log('got location!', position.coords);
+    goToKiez({ lat: position.coords.latitude, lng: position.coords.longitude });
+  }
+
+  const renderResultList = (loading, suggestions, getSuggestionItemProps) => {
+    let results = <ListItem><ListItemText primary="Suche Ergebnisse für dich..." /></ListItem>;
+
+    if (suggestions.length) {
+      results = suggestions.map(suggestion => {
+        let className = 'kr-location-search--autocomplete-item';
+        if (suggestion.active) className += ' kr-location-search--autocomplete-item__active';
+
+        console.log('suggestion', suggestion.types);
+        let locationIcon;
+
+        for(let type of suggestion.types) {
+          if (locationTypeIcons[type]) {
+            locationIcon = locationTypeIcons[type];
+            break;
+          }
+        }
+
+        if (!locationIcon) console.log('no icon found', suggestion.types);
+        if (!locationIcon) locationIcon = locationTypeIcons['default'];
+
+        return (
+          <ListItem
+            button
+            {...getSuggestionItemProps(suggestion, {
+              className,
+            })}
+          >
+            <ListItemIcon>
+              <Icon>{locationIcon}</Icon>
+            </ListItemIcon>
+            <ListItemText primary={suggestion.description} />
+          </ListItem>
+        );
+      });
+    }
+
+    return (
+      <List component="nav">
+        {results}
+      </List>
+    );
+  }
+
+  const renderResults = (loading, suggestions, getSuggestionItemProps) => {
+    if (!loading && !suggestions.length) return null;
+
+    return (
+      <Paper variant="outlined" className="kr-location-search--autocomplete">
+        {renderResultList(loading, suggestions, getSuggestionItemProps)}
+      </Paper>
+    );
+  }
+
   return (
-    <PlacesAutocomplete
-      value={address}
-      onChange={input => setAddress(input)}
-      onSelect={input => handleSelect(input)}
-      onError={e => console.log('error', e)}
-      searchOptions={searchOptions}
-    >
-      {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
-        <div>
-          <TextField 
-            {...getInputProps({
-              label: 'Dein Lieblingsort',
-              id: 'standard-basic',
-              className: 'location-search-input',
-            })}
-          />
-          <div className="autocomplete-dropdown-container">
-            {loading && <div>Suche Ergebnisse für dich...</div>}
-            {suggestions.map(suggestion => {
-              const className = suggestion.active
-                ? 'suggestion-item--active'
-                : 'suggestion-item';
-              return (
-                <div
-                  {...getSuggestionItemProps(suggestion, {
-                    className,
-                  })}
-                >
-                  <span>{suggestion.description}</span>
-                </div>
-              );
-            })}
-          </div>
-        </div>
-      )}
-    </PlacesAutocomplete>
+    <Grid container alignItems="center" spacing={1}>
+      <Grid item xs>
+        <PlacesAutocomplete
+          value={address}
+          onChange={input => setAddress(input)}
+          onSelect={input => handleSelect(input)}
+          onError={e => console.log('error', e)}
+          searchOptions={searchOptions}
+        >
+          {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+            <div className="kr-location-search">
+              <TextField 
+                {...getInputProps({
+                  label: 'Dein Lieblingsort',
+                  variant: 'outlined',
+                  className: 'kr-location-search--input',
+                  fullWidth: true,
+                  placeholder: 'Kiez, Stadtteil, Späti, Kneipe, Club, Laden, Restaurant...',
+                })}
+                InputProps={{
+                  startAdornment: <InputAdornment position="start">
+                    <Icon>search</Icon>
+                  </InputAdornment>,
+                }}
+              />
+              {renderResults(loading, suggestions, getSuggestionItemProps)}
+            </div>
+          )}
+        </PlacesAutocomplete>
+      </Grid>
+      <Grid item xs={1}>
+        <IconButton color="primary" component="span" onClick={e => getGeolocation()}>
+          <Icon>location_on</Icon>
+        </IconButton>
+      </Grid>
+    </Grid>
   );
 }
 
--- src/views/HomePage/HomePage.js
+++ src/views/HomePage/HomePage.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import LocationSearchInput from '../../components/LocationSearchInput/LocationSearchInput';
+import Navigation from '../../components/Navigation/Navigation.jsx';
+
+import { 
+  Container,
+} from '@material-ui/core';
+
+import './HomePage.scss';
+
+import KiezheroImage from '../../assets/images/kiezhero.png';
 
 const HomePage = () => {
 
 
   return (
-    <div>
-      <LocationSearchInput />
-    </div>
+    <Container maxWidth="sm" className="kr-homepage">
+      <Navigation />
+      <div className="kr-homepage--search-container">
+        <img src={KiezheroImage} className="kr-homepage--kiezhero" />
+        <LocationSearchInput /> 
+      </div>
+    </Container>
   )
 }
 
