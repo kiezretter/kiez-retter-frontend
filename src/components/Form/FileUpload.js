eff78b83e601e091f3d21d75184f4b02165c836c
Celal Basar <mail@celalbasar.com>
2020-03-21 21:53:35 +0100

    Added functionality to upload images

--- src/components/Form/FileUpload.js
+++ src/components/Form/FileUpload.js
@@ -1,39 +1,64 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import {
   Card,
   CardMedia,
   Button
 } from '@material-ui/core';
 
-const useStyles = makeStyles(theme => ({
-  mediaImage: {
-    height: 250
-  },
-}));
-const FileUpload = (props) => {
-  const classes = useStyles();
-  return (
-    <React.Fragment>
-      {props.showImagePreview && <Card>
-        <CardMedia
-          className={classes.mediaImage}
-          image="https://via.placeholder.com/250"
-          title="Foto"
-        ></CardMedia>
-      </Card>}
-      <Button
-        variant="contained"
-        component="label"
-      >
-        <i className="material-icons">cloud_upload</i>&nbsp; {props.label}
-        <input
-          type="file"
-          style={{ display: "none" }}
-        />
-      </Button>
-    </React.Fragment>
-  )
+class FileUpload extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      file: 'https://via.placeholder.com/250'
+    }
+    this.useStyles = {
+      weight: 250,
+      height: 250
+    };
+  }
+
+  handleFileUpload = (evt) => {
+    // User cancelled upload
+    if (!evt.target.files[0]) {
+      return
+    }
+    // Read file and convert it to base64
+    const reader = new FileReader()
+    reader.readAsDataURL(evt.target.files[0])
+    reader.onloadend = e => {
+      this.setState({
+        file: reader.result
+      });
+      if (!this.props.showImagePreview) {
+        console.log('=> ', this.state.file)
+      }
+    }
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        {this.props.showImagePreview && <Card>
+          <CardMedia
+            style={this.useStyles}
+            image={this.state.file}
+            title="Foto"
+          ></CardMedia>
+        </Card>}
+        <Button
+          variant="contained"
+          component="label"
+        >
+          <i className="material-icons">cloud_upload</i>&nbsp; {this.props.label}
+          <input
+            type="file"
+            onChange={this.handleFileUpload}
+            style={{ display: "none" }}
+          />
+        </Button>
+      </React.Fragment>
+    )
+  }
 }
 
 export default FileUpload;
