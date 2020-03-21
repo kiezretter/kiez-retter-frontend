23f28c52e5e0607cc390fa4dea6f638f6ecb98f3
Julian Kern <mail@juliankern.com>
2020-03-21 20:43:31 +0100

    added necessary micropages

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,22 +8,36 @@ import {
 import './Footer.scss';
 
 export default class Footer extends React.Component {
+    isActiveRoute(route) {
+        return window.location.pathname === route;
+    }
+
+    getClassesForLink(route) {
+        let classes = 'kr-footer--link';
+
+        if (this.isActiveRoute(route)) {
+            classes += ' kr-footer--link__active';
+        }
+
+        return classes;
+    }
+
     render() {
         return (
             <div className="kr-footer">
-                <Link className="kr-footer--link" href="/ueberuns">
+                <Link className={this.getClassesForLink('/ueberuns')} href="/ueberuns">
                     Über uns
                 </Link>
-                <Link className="kr-footer--link" href="/impressum">
+                <Link className={this.getClassesForLink('/impressum')} href="/impressum">
                     Impressum
                 </Link>
-                <Link className="kr-footer--link" href="/datenschutz">
+                <Link className={this.getClassesForLink('/datenschutz')} href="/datenschutz">
                     Datenschutz
                 </Link>
-                <Link className="kr-footer--link" href="/agb">
+                <Link className={this.getClassesForLink('/agb')} href="/agb">
                     AGB
                 </Link>
-                <Link className="kr-footer--link" href="/faq">
+                <Link className={this.getClassesForLink('/faq')} href="/faq">
                     FAQ
                 </Link>
             </div>
---
+++ src/views/FAQ/FAQ.jsx
@@ -0,0 +1,83 @@
+import React from 'react';
+
+import Navigation from '../../components/Navigation/Navigation.jsx';
+import Footer from '../../components/Footer/Footer.jsx';
+
+import {
+    Container,
+    ExpansionPanel,
+    ExpansionPanelSummary,
+    ExpansionPanelDetails,
+    Icon,
+    Typography,
+} from '@material-ui/core';
+
+import './FAQ.scss';
+
+const faqQuestions = [
+    {
+        question: 'Hallo Welt?',
+        answer: 'Ja bitte!'
+    },
+    {
+        question: 'Rettet ihr auch meinen Kiez?',
+        answer: 'Na auf jeden Fall!'
+    },
+    {
+        question: 'Wer seid ihr?',
+        answer: 'Ganz tolle Menschen!'
+    }
+];
+
+export default class FAQView extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            expanded: null,
+        };
+    }
+
+    get expanded() {
+        const [ expanded ] = React.useState(false);
+
+        return expanded;
+    }
+
+    handleChange(panel) {
+        const expanded = this.state.expanded === panel ? null : panel;
+
+        this.setState({ expanded });
+    }
+
+    renderQuestions() {
+        return faqQuestions.map((item, index) => (
+            <ExpansionPanel key={`question-${index}`} expanded={this.state.expanded === `question-${index}`} onChange={this.handleChange.bind(this, `question-${index}`)}>
+                <ExpansionPanelSummary
+                    expandIcon={<Icon>expand_more</Icon>}
+                    aria-controls={`question-${index}-content`}
+                    id={`question-${index}-header`}
+                >
+                    <Typography variant="subtitle2">{item.question}</Typography>
+                </ExpansionPanelSummary>
+                <ExpansionPanelDetails>
+                    <Typography>{item.answer}</Typography>
+                </ExpansionPanelDetails>
+            </ExpansionPanel>
+        ))
+    }
+
+    render() {
+        return (
+            <Container maxWidth="sm" className="kr-faq">
+                <Navigation />
+
+                <Typography variant="h2" gutterBottom>FAQ</Typography>
+                
+                {this.renderQuestions()}
+
+                <Footer />
+            </Container>
+        );
+    }
+}
