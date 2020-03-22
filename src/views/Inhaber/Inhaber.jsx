90191fc384ea3cedc3388daab71f4bd076a9207f
Celal Basar <mail@celalbasar.com>
2020-03-22 11:49:18 +0100

    Added new inhaber page

---
+++ src/views/Inhaber/Inhaber.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import Navigation from '../../components/Navigation/Navigation.jsx';
+import Footer from '../../components/Footer/Footer.jsx';
+import RegistrationForm from '../../components/Form/RegistrationForm';
+
+import {
+    Container,
+    Typography,
+} from '@material-ui/core';
+
+import './Inhaber.scss';
+
+export default class Inhaber extends React.Component {
+    render() {
+        return (
+            <Container maxWidth="md" className="kr-inhaber">
+                <Navigation />
+                <Typography variant="h2" gutterBottom>Inhaber</Typography>
+                <RegistrationForm />
+                <Footer />
+            </Container>
+        );
+    }
+}
