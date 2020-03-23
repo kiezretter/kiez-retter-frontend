ce647e35160f13612503730792cfc59d20ead366
Robin Zuschke <robin.l.zuschke@gmail.com>
2020-03-23 23:45:34 +0100

    Small changes and add virus logo

--- src/views/HomePage/AboutSection.jsx
+++ src/views/HomePage/AboutSection.jsx
@@ -13,12 +13,19 @@ export default class AboutSection extends React.Component {
         return (
             <div className="kr-homepage--about" id="ueberuns">
                 <Container maxWidth="md">
-                    <Typography variant="h4" component="h3" gutterBottom>#kiezretter</Typography>
-                    <Typography>
-                        Kiezretter ist eine Liebeserklärung an unsere Kieze – gemeinsam wollen wir dafür sorgen, dass unsere Nachbarschaft nach der Krise noch genauso bunt und vielfältig ist wie davor!<br />
-                    </Typography>
-                    {/* <Grid container spacing={3} alignItems="center" className="kr-homepage--stats">
-                        <Grid item xs>
+                    <Grid container spacing={3} alignItems="center" className="kr-homepage--stats">
+                        <Grid item sm={12} md={9}>
+                            <Typography variant="h4" component="h3" gutterBottom>#kiezretter</Typography>
+                            <Typography>
+                                Kiezretter ist eine Liebeserklärung an unsere Kieze – gemeinsam wollen wir dafür sorgen, dass unsere Nachbarschaft nach der Krise noch genauso bunt und vielfältig ist wie davor!<br />
+                            </Typography>
+                        </Grid>
+                        <Grid item sm={12} md={3}>
+                            <a href="https://wirvsvirushackathon.org" target="_blank" rel="noopener noreferrer">
+                                <img src={WirVsVirusLogo} className="kr-homepage--wirvsvirus" alt="WirVsVirusHackathon Logo" />
+                            </a>
+                        </Grid>
+                        {/* <Grid item xs>
                             <Typography>Kiezläden</Typography>
                             <Typography variant="h3" component="div" gutterBottom>9.876</Typography>
                         </Grid>
@@ -29,13 +36,8 @@ export default class AboutSection extends React.Component {
                         <Grid item xs>
                             <Typography>Kiezhelden</Typography>
                             <Typography variant="h3" component="div" gutterBottom>12.345</Typography>
-                        </Grid>
-                        <Grid item sm={12} md={3}>
-                            <a href="https://wirvsvirushackathon.org" target="_blank" rel="noopener noreferrer">
-                                <img src={WirVsVirusLogo} className="kr-homepage--wirvsvirus" alt="WirVsVirusHackathon Logo" />
-                            </a>
-                        </Grid>
-                    </Grid> */}
+                        </Grid> */}
+                    </Grid>
                 </Container>
             </div>
         );
