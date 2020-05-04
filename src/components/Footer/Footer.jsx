b0308fc84a6da5b2a01518a48dbe7e6b57ab1ff8
Robin Zuschke <robin.l.zuschke@gmail.com>
2020-05-04 10:53:13 +0200

    Implement partner logos into footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import PayPalLogo from '../../assets/images/paypal_partner_logo.png';
+import GoogleLogo from '../../assets/images/google_partner_logo.png';
+import AWSLogo from '../../assets/images/aws_partner_logo.png';
 
 import {
     Link,
@@ -27,6 +30,14 @@ export default class Footer extends React.Component {
         return (
             <div className="kr-footer">
                 <Container maxWidth="sm">
+                <Typography variant="body2" style={{ display: "flex",  flexWrap: "wrap", justifyContent: "center", marginBottom: '10px'}}>
+                        <p>Mit freundlicher Unterstützung von</p>
+                    </Typography>
+                    <Typography variant="body2" style={{ display: "flex",  flexWrap: "wrap", justifyContent: "center", alignItems: "center", marginBottom: '20px'}}>
+                        <img src={PayPalLogo} alt="paypal-partner-logo" className="partner-logo"/>
+                        <img src={AWSLogo} alt="aws-partner-logo" className="partner-logo"/>
+                        <img src={GoogleLogo} alt="google-partner-logo" className="partner-logo"/>
+                    </Typography>
                     <Typography variant="body2" style={{ display: "flex",  flexWrap: "wrap", justifyContent: "center"}}>
                         <Link className={this.getClassesForLink('/ueberuns')} href="/ueberuns">
                             Über uns
